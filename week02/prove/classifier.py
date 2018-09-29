d291cbf1bcd3699844485748afd769c8830c93bd
LeVan Smith <levictorsmith@gmail.com>
2018-09-29 12:19:26 -0600

    Add k-nearest neighbor classifier

--- week02/prove/classifier.py
+++ week02/prove/classifier.py
@@ -1,8 +1,10 @@
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+from collections import Counter
 
-"""
-Hard-coded Classifier
-"""
+###############################
+# Hard-coded Classifier
+###############################
 class HardCodedClassifier():
   def fit(self, training_data, training_targets):
     return HardCodedModel()
@@ -12,9 +14,9 @@ class HardCodedModel():
     results = [0 for _ in test_data]
     return results
 
-"""
-K-Nearest Neighbor Classifier
-"""
+###############################
+# K-Nearest Neighbor Classifier
+###############################
 class KNeighborsClassifier():
   def __init__(self, neighbors):
     self.neighbors = neighbors
@@ -22,12 +24,31 @@ class KNeighborsClassifier():
   def fit(self, training_data, training_targets):
     self.data = training_data
     self.targets = training_targets
-    return KNeighborsModel()
+    std_scale = StandardScaler().fit(self.data)
+    standardized = std_scale.transform(self.data)
+    return KNeighborsModel(standardized, self.targets, self.neighbors)
 
 class KNeighborsModel():
-  def predict(self, test_data):
-    results = [0 for _ in test_data]
-    return results
-  
-  def get_distance(self):
+  def __init__(self, training_data, training_targets, neighbors):
+    self.data = training_data
+    self.targets = training_targets
+    self.neighbors = neighbors
     pass
+
+  def predict(self, test_data):
+    std_scale = StandardScaler().fit(test_data)
+    standarized = std_scale.transform(test_data)
+
+    nearest = []
+    for x in standarized:
+      distances = [self.get_distance(x, n) for n in self.data]
+      neighbors = self.get_nearest_neighbors(distances, self.targets)
+      nearest.append(np.argmax(np.bincount(neighbors)))
+    return np.array(nearest)
+  
+  def get_distance(self, a, b):
+    return np.sum((a - b) ** 2)
+  
+  def get_nearest_neighbors(self, data, targets):
+    indices = np.argsort(data, axis=0)
+    return targets[indices[:self.neighbors]]
--- week02/prove/main.py
+++ week02/prove/main.py
@@ -3,14 +3,22 @@ import numpy as np
 from args import parse_args
 from data import Dataset
 from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier as LibraryKNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from classifier import HardCodedClassifier, HardCodedModel, KNeighborsClassifier, KNeighborsModel
 
-def get_algorithm(algorithm):
+def get_algorithm(args):
+  algorithm = args.algorithm
   return {
     'hard_coded': HardCodedClassifier(),
     'naive_bayes': GaussianNB(),
-    'k_nearest': KNeighborsClassifier(3),
+    'k_nearest': KNeighborsClassifier(int(args.neighbors)),
+  }[algorithm]
+
+def get_library_version(args):
+  algorithm = args.algorithm
+  return {
+    'k_nearest': LibraryKNeighborsClassifier(int(args.neighbors))
   }[algorithm]
 
 def get_train_test_data(filename, test_size):
@@ -19,12 +27,21 @@ def get_train_test_data(filename, test_size):
 
 def main():
   args = parse_args()
-  classifier = get_algorithm(args.algorithm)
   data_train, data_test, target_train, target_test = get_train_test_data(filename=args.file, test_size=args.test_size)
+
+  classifier = get_algorithm(args)
   model = classifier.fit(data_train, target_train)
   targets_predicted = model.predict(data_test)
   correct = 1 - np.mean(target_test != targets_predicted)
   print("Accuracy: {:.2%}".format(correct))
+
+  lib_classifier = get_library_version(args)
+  lib_model = lib_classifier.fit(data_train, target_train)
+  lib_targets_predicted = lib_model.predict(data_test)
+  lib_correct = 1 - np.mean(target_test != lib_targets_predicted)
+  print("Library Accuracy: {:.2%}".format(lib_correct))
+
+  print("Difference from Library version: {:+.2%}".format(correct - lib_correct))
 
 if __name__ == '__main__':
   main()
