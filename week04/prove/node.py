012e3e8d947626969b1e94ca1a64aa44ef306575
LeVan Smith <levictorsmith@gmail.com>
2018-10-21 00:47:33 -0600

    Add prove 4 report

--- week04/prove/node.py
+++ week04/prove/node.py
@@ -7,14 +7,6 @@ class Node():
       for key, node in children.items():
         self.add_child(key, node)
   
-  # def __str__(self, level=0):
-  #   ret = "\t"*level+repr(self.value)+"\n"
-  #   for key, child in self.children:
-  #       ret += child.__str__(level+1)
-  #   return ret
-  # def __repr__(self):
-  #   return '<tree node representation>'
-  
   def add_child(self, key, node):
     assert isinstance(node, Node)
     self.children[key] = node
